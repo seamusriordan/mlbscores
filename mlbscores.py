b3e6723e324b76a76b095b489fe5c0e925491a6a
Seamus Riordan <seamus@riordan.io>
2016-05-01 20:26:51 -0400

    Fix PA calculation

--- mlbscores.py
+++ mlbscores.py
@@ -131,7 +131,7 @@ def printboxscore(game):
                          game[batters["team_flag"]+"_team_name"]  )
                           
         # batter variables we want to sum
-        keystosum = ["ab", "h", "bb", "r", "hr", "so"]
+        keystosum = ["ab", "h", "bb", "r", "hr", "so", "hbp", "sac"]
         sums = {}
         for k in keystosum: 
             sums[k] = 0
@@ -140,7 +140,7 @@ def printboxscore(game):
             try:
                 sys.stdout.write("%2s %-20s %3d %3d %3d %3d %3d %3d  %5.3f  %5.3f\n" % (
                     p["pos"], p["name_display_first_last"], \
-                    int(p["ab"]) + int(p["bb"]), int(p["h"]), int(p["bb"]), int(p["r"]),\
+                    int(p["ab"]) + int(p["bb"]) + int(p["hbp"]) + int(p["sac"]), int(p["h"]), int(p["bb"]), int(p["r"]),\
                     int(p["hr"]), int(p["so"]), float(p['obp']), float(p['obp']) + float(p['slg'])) )
             except Exception as e:
                 print e
@@ -148,7 +148,7 @@ def printboxscore(game):
             for k in sums.keys():
                 sums[k] += float(p[k])
         sys.stdout.write("   %-20s %3d %3d %3d %3d %3d %3d\n\n" % ("TOTAL", \
-            int(sums["ab"]) + int(sums["bb"]), int(sums["h"]), int(sums["bb"]), int(sums["r"]),\
+            int(sums["ab"]) + int(sums["bb"]) + int(sums["hbp"]) + int(sums["sac"]), int(sums["h"]), int(sums["bb"]), int(sums["r"]),\
             int(sums["hr"]), int(sums["so"]) ))
 
     
